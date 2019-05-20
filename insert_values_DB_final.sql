8264a2703e41d421c3b6c335780ba4de2fa98471
LAPTOP-T0G2AM3E\LeiX <leix0053@stud.kea.dk>
2019-05-20 21:48:22 +0200

    insert values to Products

---
+++ insert_values_DB_final.sql
@@ -0,0 +1,27 @@
+select*from Products
+
+delete from Products
+where Products.category = 'Shoes'  
+
+insert into Products values('Venom Low Sneakers', 'Cow leather 90% / Polyester 10%', 'Yellow', 'Fila', 'Shoes', 'M', 'Sneakers');
+insert into Products values('Venom Low Sneakers', 'Cow leather 90% / Polyester 10%', 'Yellow', 'Fila', 'Shoes', 'F', 'Sneakers');
+insert into Products values('Premium Boots', 'Cow leather 56%', 'Yellow', 'Timberland', 'Shoes', 'M', 'Boots');
+insert into Products values('Premium Boots', 'Cow leather 56%', 'Yellow', 'Timberland', 'Shoes', 'F', 'Boots');
+insert into Products values('Premium Boots', 'Cow leather 56%', 'White', 'Timberland', 'Shoes', 'M', 'Boots');
+insert into Products values('Premium Boots', 'Cow leather 56%', 'Black', 'Timberland', 'Shoes', 'F', 'Boots');
+insert into Products values('1461 Shoes', 'Leather 90% / Polyester 10%', 'Black', 'Dr Martens', 'Shoes', 'F', 'Oxfords');
+insert into Products values('1461 Shoes', 'Leather 90% / Polyester 10%', 'Black', 'Dr Martens', 'Shoes', 'M', 'Oxfords');
+insert into Products values('High Top Sneakers', 'Cow leather 100%', 'White', 'Buffalo', 'Shoes', 'M', 'Sneakers');
+insert into Products values('High Top Sneakers', 'Cow leather 100%', 'White', 'Buffalo', 'Shoes', 'F', 'Sneakers');
+insert into Products values('Flip Cord Flip-Flops', 'Ethylene Vinyl Acetate 100% / Polyester 10%', 'Black / Yellow', 'Flip Cord', 'Shoes', 'F', 'Flip-Flops');
+insert into Products values('Flip Cord Flip-Flops', 'Ethylene Vinyl Acetate 100% / Polyester 10%', 'Black / Yellow', 'Flip Cord', 'Shoes', 'M', 'Flip-Flops');
+insert into Products values('Anaheim Shoes', 'Cotton 100%', 'Pink / White', 'Vans', 'Shoes', 'F', 'Sneakers');
+
+insert into Products values('Chelsea Boots', 'Cow leather 100%', 'Black', 'Dr. Martens', 'Shoes', 'F', 'Boots');
+insert into Products values('Chelsea Boots', 'Cow leather 100%', 'Black', 'Dr. Martens', 'Shoes', 'M', 'Boots');
+insert into Products values('DR Mono Low', 'Cow leather 100%', 'Black', 'Dr. Martens', 'Shoes', 'M', 'Oxfords');
+insert into Products values('DR Mono Low', 'Cow leather 100%', 'Black', 'Dr. Martens', 'Shoes', 'F', 'Oxfords');
+insert into Products values('Thick-soled Leather Sneakers', 'Leather 100%', 'Black', 'Cos', 'Shoes', 'M', 'Oxfords');
+insert into Products values('Thick-soled Leather Sneakers', 'Leather 100%', 'Blue', 'Cos', 'Shoes', 'F', 'Oxfords');
+insert into Products values('Thick-soled Leather Sneakers', 'Leather 100%', 'White', 'Cos', 'Shoes', 'M', 'Oxfords');
+
