8a4e169e3771d2cee526720cbdb1c0cfc5efaa2d
Bilbo Swaggins - lenovo <rm.cehlar@gmail.com>
2019-05-24 14:57:47 +0200

    sql function tests

--- sql queries/test.sql
+++ sql queries/test.sql
@@ -1,2 +1,5 @@
-SELECT * FROM getOrderedItems(23);
+SELECT * FROM getOrderedItems(21);
 PRINT dbo.getFavoriteCategory(23);
+SELECT * FROM getCategoryFrequency(21);
+SELECT * FROM getColorFrequency(21);
+SELECT * FROM getMaterialFrequency(21);
